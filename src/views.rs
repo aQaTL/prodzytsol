e5de83c9d1d0e4341153c8920956b367d9d75abd
aQaTL <mmsoltys@outlook.com>
2021-03-14 00:03:37 +0100

    Add Lato fonts

--- src/views.rs
+++ src/views.rs
@@ -69,6 +69,11 @@ fn header(size: HeaderSize, txt: &str) -> Element {
         // .height(Length::Fill)
         .size(size.to_font_size())
         .color(WHITE)
+        // .font(Font::External {
+        //     name: "Lato-Bold",
+        //     bytes: fonts::LATO_BOLD,
+        // })
+        .font(fonts::LATO_BOLD)
         .horizontal_alignment(HorizontalAlignment::Center)
         .vertical_alignment(VerticalAlignment::Center)
         .into()
@@ -82,4 +87,34 @@ fn text(txt: &str) -> Element {
         .horizontal_alignment(HorizontalAlignment::Center)
         .vertical_alignment(VerticalAlignment::Center)
         .into()
+}
+
+#[allow(dead_code)]
+pub mod fonts {
+    const fn new_external_font(name: &'static str, bytes: &'static [u8]) -> iced::Font {
+        iced::Font::External { name, bytes }
+    }
+    macro_rules! font {
+        ($($name: ident : $filename: expr $(,)? ),*) => {
+            $(
+                pub static $name: iced::Font = new_external_font(
+                    $filename,
+                    include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/fonts/", $filename)),
+                );
+            )*
+        };
+    }
+
+    font![
+        LATO_BLACK: "Lato-BlackItalic.ttf",
+        LATO_BLACK_ITALIC: "Lato-BlackItalic.ttf",
+        LATO_BOLD: "Lato-Bold.ttf",
+        LATO_BOLD_ITALIC: "Lato-BoldItalic.ttf",
+        LATO_ITALIC: "Lato-Italic.ttf",
+        LATO_LIGHT: "Lato-Light.ttf",
+        LATO_LIGHT_ITALIC: "Lato-LightItalic.ttf",
+        LATO_REGULAR: "Lato-Regular.ttf",
+        LATO_THIN: "Lato-Thin.ttf",
+        LATO_THINITALIC: "Lato-ThinItalic.ttf",
+    ];
 }
